a0f1bab6c1d3bf3227cbe8144a01d94e6facb6b1
= <=>
2022-06-29 03:36:31 +0700

    membuat create user

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use PDF;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Query\Builder;
 
@@ -13,5 +14,40 @@ class AdminController extends Controller
     function index()
     {
         return view('ViewAdmin.index',['tittle' => 'Home Page Admin']);
+    }
+
+    public function createUser()
+    {
+        return view('ViewAdmin.createDataUser',['tittle' => 'Create Data User']);
+    }
+
+    public function storeUser(Request $request)
+    {
+       
+       //melakukan validasi data
+       $request->validate([
+        'name' => 'required',
+        'email' => ['required', 'email:dns', 'unique:users,email,'],
+        'notelp' => ['required', 'numeric', 'unique:users,notelp,'],
+        'alamat' => 'required',
+        'level' => 'required',
+        'password' => 'required',
+        'foto'=>'required',]);
+
+        if($request->file('foto')){
+            $image_name = $request->file('foto')->store('image', 'public');
+        }
+
+        $user = new User;
+        $user->name = $request->get('name');
+        $user->email = $request->get('email');
+        $user->foto = $image_name;
+        $user->notelp = $request->get('notelp');
+        $user->alamat = $request->get('alamat');
+        $user->level = $request->get('level');
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        return redirect('/homeAdmin') -> with('success', 'Data berhasil Ditambahkan');
     }
 }
--- resources/views/ViewAdmin/sidebar.blade.php
+++ resources/views/ViewAdmin/sidebar.blade.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4" style="height: 100vh">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-      <img src="{{ asset('src/img/brandlogo.jpg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Toko Fast</span>
+      <img src="{{ asset('assets/img/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Ganendra Giri</span>
     </a>
 
     <!-- Sidebar -->
