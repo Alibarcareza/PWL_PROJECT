74d7563e274db4a737e401cce1a1b592d1b88e2e
= <=>
2022-06-29 05:29:08 +0700

    store alat

--- app/Http/Controllers/AlatController.php
+++ app/Http/Controllers/AlatController.php
@@ -20,5 +20,31 @@ class AlatController extends Controller
 
     }
     // fungsi store data user
+    public function storeAlat(Request $request)
+    {
+       //melakukan validasi data
+        $request->validate([
+        'nama' => 'required',
+        'kategori' => ['required'],
+        'merk' => ['required'],
+        'jumlah' => 'required',
+        'gambar'=>'required',
+        ]);
+
+        if($request->file('gambar')){
+            $image_name = $request->file('gambar')->store('image', 'public');
+        }
+        
+        $alat = new Alat;
+        $alat -> nama = $request->get('nama');
+        $gambar = $request->file('gambar')->store('gambar', 'public');
+        $alat -> gambar = $gambar;
+        $alat -> kategori = $request->get('kategori');
+        $alat -> merk = $request->get('merk');
+        $alat -> jumlah = $request->get('jumlah');
+        $alat -> save();
+
+        return redirect('/homeAdmin') -> with('success', 'Data Barang berhasil Ditambahkan');
+    }
    
 }
