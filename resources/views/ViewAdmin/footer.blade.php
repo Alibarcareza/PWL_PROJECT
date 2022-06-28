f0f4c3169c2274c6737fc077d2f4f1345cd2834a
= <=>
2022-06-29 01:45:17 +0700

    menambahkan header,nav,side,foot

---
+++ resources/views/ViewAdmin/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2022</strong> <a>WEBSITE GG | PEMROGRAMAN WEB LANJUT </a>All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <a>All rights reserved.</a> 
+  </footer>
