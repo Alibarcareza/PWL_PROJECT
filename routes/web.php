c0456fd00b2417a48434ce4a116e5d3cefe14cac
= <=>
2022-06-29 02:10:30 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomePageController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\AdminController;
 use App\Http\Middleware\CekLevel;
 
 /*
