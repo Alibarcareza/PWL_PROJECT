157be84515f7b77c1871545876d9038505b4bf83
Zaidan <zaidan@Zaidans-MacBook-Air.local>
2022-12-04 19:33:22 +0700

    fix

--- resources/views/AuthPage/script.blade.php
+++ resources/views/AuthPage/script.blade.php
@@ -1,6 +1,6 @@
 <!-- jQuery -->
-<script src="{{ asset('adminLTE/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{ asset('AdminLTE/plugins/jquery/jquery.min.js')}}"></script>
 <!-- Bootstrap 4 -->
-<script src="{{ asset('adminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{ asset('AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset('adminLTE/dist/js/adminlte.min.js')}}"></script>
+<script src="{{ asset('AdminLTE/dist/js/adminlte.min.js')}}"></script>
